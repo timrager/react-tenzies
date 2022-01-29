710492f03eb1fd0a48841ad0f25a1e1289132a5c
Tim Rager <tim.rager@gmail.com>
2022-01-29 10:38:49 -0800

    added roll btn

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ const App = () => {
         <div className="dice-container">
           {diceElements}
         </div>
+        <div className="roll-btn" onClick={() => setDice(allNewDice)}> Roll </div>
       </main>
   );
 };
