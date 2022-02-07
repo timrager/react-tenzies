1f2458816b3666fe13e3670c64c2257c410f7d74
Tim Rager <tim.rager@gmail.com>
2022-02-07 12:00:24 -0800

    can hold dice and roll others

--- src/App.js
+++ src/App.js
@@ -21,15 +21,32 @@ const App = () => {
   const [dice, setDice] = useState(allNewDice())
 
   const diceElements = dice.map(die => (
-    <Dice key={die.id} value={die.value} isHeld={die.isHeld}/>
+    <Dice
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)} 
+    />
   ))
+
+  const holdDice = (id) => {
+    setDice(oldDice => oldDice.map(die => {
+      return die.id === id ? {...die, isHeld: !die.isHeld} : die
+    }))
+  }
+
+  const rollDice = () => {
+    setDice(checkDice => checkDice.map(die => {
+      return die.isHeld ? die : {...die, id: nanoid(), value: Math.ceil(Math.random() *6)}
+    }))
+  }
 
   return (
       <main className="board">
         <div className="dice-container">
           {diceElements}
         </div>
-        <div className="roll-btn" onClick={() => setDice(allNewDice)}> Roll </div>
+      <div className="roll-btn" onClick={rollDice}> Roll </div>
       </main>
   );
 };
--- src/Dice.js
+++ src/Dice.js
@@ -3,14 +3,15 @@ import React from 'react';
 
 const Dice = (props) => {
 
+
     return (
         <div className="die-face">
             {
                 props.isHeld
                 ?
-                    <h2 className="dice dice-hold" >{props.value}</h2>
+                    <h2 className="dice dice-hold" onClick={props.holdDice}>{props.value}</h2>
                 :
-                    <h2 className="dice">{props.value}</h2>
+                    <h2 className="dice" onClick={props.holdDice}>{props.value}</h2>
             }
             
         </div>
